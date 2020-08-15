f62cfebee44911bec6d70b7e0e0b2df1cb0b622e
Wojtek Nowak <billnewmann@gmail.com>
2020-08-15 16:58:11 +0100

    Separate endpoints and modify server.js accordingly

--- server.js
+++ server.js
@@ -5,15 +5,17 @@ const hbs = require('express-handlebars');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const session = require('express-session');
-const passportConfig = require('./config/passport');
-
-
+const passportSetup = require('./config/passport');
 
 const app = express();
 
-
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
+
+
+app.use(session({ secret: 'anything' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(cors());
 app.use(express.json());
@@ -24,26 +26,8 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/user/logged', (req, res) => {
-  res.render('logged');
-});
-
-app.get('/user/no-permission', (req, res) => {
-  res.render('noPermission');
-});
-
-app.get('/auth/google',
-  passport.authenticate('google', { scope: ['email', 'profile'] }));
-
-app.get('/auth/google/callback', (req, res) => {
-  res.send(`I'm back from Google!`);
-});
-
-app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/user/no-permission' }),
-  (req, res) => {
-    res.redirect('/user/logged');
-  }
-);
+app.use('/auth', require('./routes/auth.routes'));
+app.use('/user', require('./routes/user.routes'));
 
 app.use('/', (req, res) => {
   res.status(404).render('notFound');
@@ -53,6 +37,3 @@ app.listen('8000', () => {
   console.log('Server is running on port: 8000');
 });
 
-app.use(session({ secret: 'anything' }));
-app.use(passport.initialize());
-app.use(passport.session());
