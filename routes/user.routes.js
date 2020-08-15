a57f463ba363f0d6d0f4ab4a294e3e3b7f4c8dd6
Wojtek Nowak <billnewmann@gmail.com>
2020-08-15 19:08:34 +0100

    Add new endpoints, with authentication

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -12,5 +12,19 @@ router.get('/no-permission', (req, res) => {
   res.render('noPermission');
 });
 
+router.get('/profile', (req, res) => {
+    if(req.user){
+      res.render('profile');
+    }
+    else{res.render('noPermission')};
+  });
+  
+  router.get('/profile/settings', (req, res) => {
+    if(req.user){
+      res.render('settings');
+    }
+    else{res.render('noPermission')};
+  });
+
 
 module.exports = router;
