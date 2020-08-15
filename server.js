0588feff61dece1902f75747fdd62fcd736fa9dd
Wojtek Nowak <billnewmann@gmail.com>
2020-08-15 14:42:46 +0100

    start preparing server.js

--- server.js
+++ server.js
@@ -2,8 +2,29 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const hbs = require('express-handlebars');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
 
 const app = express();
+
+passport.use(new GoogleStrategy({
+  clientID: '265373913151-v4bbr67cjjn96u7t5jbu2ns5rq8j649d.apps.googleusercontent.com',
+  clientSecret: 'HbI5j4JjHcQ_xvGMz8uZEHx4',
+  callbackURL: 'http://localhost:8000/auth/callback'
+  }, (accessToken, refreshToken, profile, done) => {
+    done(null, profile);
+  }));
+
+// serialize user when saving to session
+passport.serializeUser((user, serialize) => {
+  serialize(null, user);
+});
+
+// deserialize user when reading from session
+passport.deserializeUser((obj, deserialize) => {
+  deserialize(null, obj);
+});
 
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
@@ -32,3 +53,7 @@ app.use('/', (req, res) => {
 app.listen('8000', () => {
   console.log('Server is running on port: 8000');
 });
+
+app.use(session({ secret: 'anything' }));
+app.use(passport.initialize());
+app.use(passport.session());
